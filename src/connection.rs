0088c17d5094dbebf0962cc4cec40acd9e51f51c
Luis Fernando Batels <luisfbatels@gmail.com>
2020-05-21 22:59:31 -0300

    Connection working
    Connection, using rust, working!

--- src/connection.rs
+++ src/connection.rs
@@ -25,32 +25,33 @@ pub fn open(host: String, port: u32, db_name: String, user: String, pass: String
 
         let mem_alloc = 1 + user.len() + 2 + pass.len() + 2;
         let mut dpb: *mut c_char = libc::malloc(mem_alloc) as *mut c_char;
+        let mut dpb_len = 1 as c_short;
+        let mut dpb_len_p = &mut dpb_len as *mut c_short;
         
         *dpb = ibase::isc_dpb_version1 as c_char;
 
         let c_user = match CString::new(user.clone()) {
-            Ok(c) => c.as_ptr() as *const c_char,
+            Ok(c) => c.into_raw(),
             Err(e) => return Err(FbError { code: -1, msg: e.to_string() })
         };
-        ibase::isc_modify_dpb(&mut dpb, &mut 0, ibase::isc_dpb_user_name as u16, c_user, user.len() as c_short);
+        ibase::isc_modify_dpb(&mut dpb, dpb_len_p, ibase::isc_dpb_user_name, c_user, user.len() as c_short);
 
         let c_pass = match CString::new(pass.clone()) {
-            Ok(c) => c.as_ptr() as *const c_char,
+            Ok(c) => c.into_raw(),
             Err(e) => return Err(FbError { code: -1, msg: e.to_string() })
         };
-        ibase::isc_modify_dpb(&mut dpb, &mut 0, ibase::isc_dpb_password as u16, c_pass, pass.len() as c_short);
+        ibase::isc_modify_dpb(&mut dpb, dpb_len_p, ibase::isc_dpb_password, c_pass, pass.len() as c_short);
 
         let host_db = format!("{}:{}", host, db_name);
         let c_host_db = match CString::new(host_db.clone()) {
-            Ok(c) => c.as_ptr() as *const c_char,
+            Ok(c) => c.into_raw(),
             Err(e) => return Err(FbError { code: -1, msg: e.to_string() })
         };
         
         let status: *mut ibase::ISC_STATUS_ARRAY = libc::malloc(mem::size_of::<ibase::ISC_STATUS_ARRAY>()) as *mut ibase::ISC_STATUS_ARRAY;
-        let host_db_len = host_db.len() as c_short;
-        let dpb_len = 0 as c_short;
 
-        if ibase::isc_attach_database(status, host_db_len, c_host_db, handle, dpb_len, dpb) != 0 {
+        println!("{} {}", host_db, dpb_len);
+        if ibase::isc_attach_database(status, 0, c_host_db, handle, dpb_len, dpb) != 0 {
             return Err(FbError::from_status(status)); 
         }
     }
