b242acf87e9da3bc5bec8fa5817975cdb0fdcec8
jairinhohw <jairinhowiethan@gmail.com>
2020-07-28 16:28:42 -0300

    Simple reconnection example

---
+++ examples/reconnection.rs
@@ -0,0 +1,54 @@
+//!
+//! Rust Firebird Client
+//!
+//! Example of simple reconnection
+//!
+//! You need create a database named test.fdb:
+//!
+
+use rsfbclient::{prelude::*, ConnectionBuilder};
+use std::time::Duration;
+
+fn main() {
+    let builder = {
+        #[cfg(not(feature = "dynamic_loading"))]
+        let mut builder = ConnectionBuilder::default();
+
+        #[cfg(feature = "dynamic_loading")]
+        let mut builder =
+            ConnectionBuilder::with_client("./fbclient.lib").expect("Unable to find fbclient.lib");
+
+        builder
+            .host("localhost")
+            .db_name("test.fdb")
+            .user("SYSDBA")
+            .pass("masterkey");
+
+        builder
+    };
+
+    let mut conn = builder.connect().unwrap();
+
+    loop {
+        match conn.query_first("SELECT rand() FROM RDB$DATABASE", ()) {
+            Ok(Some((resp,))) => {
+                let resp: f64 = resp;
+
+                println!("Resp: {}", resp);
+            }
+
+            Err(e) => {
+                eprintln!("Error: {}", e);
+
+                match builder.connect() {
+                    Ok(new_conn) => conn = new_conn,
+                    Err(e) => eprintln!("Error on reconnect: {}", e),
+                }
+            }
+
+            _ => panic!("Select returned nothing"),
+        }
+
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
