111f6838a88c707ba1a96f277d62c385f89c29e9
jairinhohw <jairinhowiethan@gmail.com>
2020-08-05 02:02:40 -0300

    Removed unnecessary code

--- build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    #[cfg(not(feature = "dynamic_loading"))]
-    println!("cargo:rustc-link-lib=dylib=fbclient");
-}
--- src/ibase/mod.rs
+++ src/ibase/mod.rs
@@ -6,10 +6,8 @@
 
 mod common;
 mod consts;
-mod functions;
 mod wire;
 
 pub use common::*;
 pub use consts::*;
-pub use functions::*;
 pub use wire::*;
