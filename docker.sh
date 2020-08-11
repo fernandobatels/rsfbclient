1cfa45482b5fea21ce99f7f5790a1c87131d36e0
Juarez Rudsatz <juarez.rudsatz@ceabs.net>
2020-08-11 18:24:45 -0300

    create container for v2 v3 and v4 on docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+set -e
+
+case $2 in
+    ""|v3|latest) FIREBIRD_VERSION='latest' ;;
+    v2) FIREBIRD_VERSION='2.5-ss' ;;
+    v4) FIREBIRD_VERSION='4.0' ;;
+    *) echo '# Firebird version must be: v2 v3 or v4' && exit 1 ;;
+esac
+
 case $1 in
     start) docker start firebirdsql ;;
 
@@ -9,7 +18,7 @@ case $1 in
 
     test) echo 'select * from rdb$database;' | isql-fb -bail -quiet -z -user my_user -password my_password 'localhost:test.fdb' && echo 'SUCCESS' || echo "FAILURE" ;;
 
-    create) docker run --detach --publish '3050:3050'  --name 'firebirdsql'  --env 'ISC_PASSWORD=masterkey'  --env 'FIREBIRD_DATABASE=test.fdb' --env 'FIREBIRD_USER=my_user' --env 'FIREBIRD_PASSWORD=my_password' 'jacobalberty/firebird:latest' ;;
+    create) docker run --detach --publish '3050:3050'  --name 'firebirdsql'  --env 'ISC_PASSWORD=masterkey'  --env 'FIREBIRD_DATABASE=test.fdb' --env 'FIREBIRD_USER=my_user' --env 'FIREBIRD_PASSWORD=my_password' "jacobalberty/firebird:${FIREBIRD_VERSION}" ;;
 
     * )	echo '# Usage:\n\t./docker.sh <create | start | stop | remove>\n' ;;
 esac
