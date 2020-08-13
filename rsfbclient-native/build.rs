d090c081c2d6ed9ea80c60c7996524756f3f8156
Juarez Rudsatz <juarez.rudsatz@ceabs.net>
2020-08-13 20:57:41 -0300

    ci: change build.rs with cargo:rustc-link-lib=fbclient

--- rsfbclient-native/build.rs
+++ rsfbclient-native/build.rs
@@ -1,4 +1,6 @@
 fn main() {
     #[cfg(feature = "linking")]
-    println!("cargo:rustc-link-lib=dylib=fbclient");
+    // println!("cargo:rustc-link-lib=dylib=fbclient");
+    // println!("cargo:rustc-link-lib=fbclient");
+    println!("cargo:rustc-link-search=native=C:\\Program Files\\Firebird\\Firebird_3_0\\lib\\");    
 }
