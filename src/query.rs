4e6972a14e6ddee4f592df165a7a3c3d01fe93b1
Luis Fernando Batels <luisfbatels@gmail.com>
2020-10-14 23:10:49 -0300

    Docs about the new method
    Docs about the diference between query and execute_returnable created

--- src/query.rs
+++ src/query.rs
@@ -8,7 +8,10 @@ use rsfbclient_core::{FbError, FromRow, IntoParams};
 
 /// Implemented for types that can be used to execute sql queries
 pub trait Queryable {
-    /// Returns the results of the query as an iterator
+    /// Returns the results of the query as an iterator.
+    ///
+    /// The query must be return an open cursor, so for cases like 'insert .. returning'
+    /// you will need to use the [execute_returnable](prelude/trait.Execute.html#tymethod.execute_returnable) method instead.
     ///
     /// Use `()` for no parameters or a tuple of parameters
     fn query_iter<'a, P, R>(
@@ -20,7 +23,10 @@ pub trait Queryable {
         P: IntoParams,
         R: FromRow + 'static;
 
-    /// Returns the results of the query as a `Vec`
+    /// Returns the results of the query as a `Vec`.
+    ///
+    /// The query must be return an open cursor, so for cases like 'insert .. returning'
+    /// you will need to use the [execute_returnable](prelude/trait.Execute.html#tymethod.execute_returnable) method instead.
     ///
     /// Use `()` for no parameters or a tuple of parameters
     fn query<'a, P, R>(&'a mut self, sql: &str, params: P) -> Result<Vec<R>, FbError>
@@ -31,7 +37,10 @@ pub trait Queryable {
         self.query_iter(sql, params)?.collect()
     }
 
-    /// Returns the first result of the query, or None
+    /// Returns the first result of the query, or None.
+    ///
+    /// The query must be return an open cursor, so for cases like 'insert .. returning'
+    /// you will need to use the [execute_returnable](prelude/trait.Execute.html#tymethod.execute_returnable) method instead.
     ///
     /// Use `()` for no parameters or a tuple of parameters
     fn query_first<'a, P, R>(&'a mut self, sql: &str, params: P) -> Result<Option<R>, FbError>
@@ -52,8 +61,11 @@ pub trait Execute {
     where
         P: IntoParams;
 
-    /// Execute a query that will return data,
-    /// like the 'insert ... returning ..' or 'execute procedure'
+    /// Execute a query that will return data, like the 'insert ... returning ..' or 'execute procedure'.
+    ///
+    /// This method is designated for use in cases when you don't have
+    /// a cursor to fetch. [This link](https://www.ibexpert.net/ibe/pmwiki.php?n=Doc.DataManipulationLanguage#EXECUTEBLOCKStatement)
+    /// explain the Firebird behavior for this cases.
     ///
     /// Use `()` for no parameters or a tuple of parameters
     fn execute_returnable<P, R>(&mut self, sql: &str, params: P) -> Result<R, FbError>
