1614381ff5858c0f755657ef480f2058ffb3f460
silvioprog <silvioprog@gmail.com>
2021-02-22 17:58:42 -0300

    Use only pure_rust in the pool example

--- r2d2_firebird/examples/pool.rs
+++ r2d2_firebird/examples/pool.rs
@@ -14,15 +14,6 @@ use std::{sync::Arc, thread, time::Duration};
 
 fn main() {
     let builder = {
-        #[cfg(feature = "linking")]
-        let mut builder = rsfbclient::builder_native().with_dyn_link().with_remote();
-
-        #[cfg(feature = "dynamic_loading")]
-        let mut builder = rsfbclient::builder_native()
-            .with_dyn_load("./fbclient.lib")
-            .with_remote();
-
-        #[cfg(feature = "pure_rust")]
         let mut builder = rsfbclient::builder_pure_rust();
 
         builder
